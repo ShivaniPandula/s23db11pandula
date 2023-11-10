4fa434b23df172771f4a001e2f7be6aac2948d88
ShivaniPandula <143038757+ShivaniPandula@users.noreply.github.com>
2023-11-10 02:06:16 -0600

    part2

--- controllers/AirlineTicket.js
+++ controllers/AirlineTicket.js
@@ -76,22 +76,23 @@ exports.AirlineTicket_detail = async function (req, res) {
     }
 };
 
-//Handle AirlineTicket update form on PUT
-exports.AirlineTicket_update_put = async function (req, res) {
-    console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`)
+//Handle Vehicle update form on PUT
+exports.AirlineTicket_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
     try {
-        let toUpdate = await AirlineTicket.findById(req.params.id)
-        // Do updates of properties
-        if (req.body.AirlineTicket_passengerName)
-            toUpdate.AirlineTicket_passengerName = req.body.AirlineTicket_passengerName;
-        if (req.body.AirlineTicket_ticketNumber) toUpdate.AirlineTicket_ticketNumber = req.body.AirlineTicket_ticketNumber;
-        if (req.body.AirlineTicket_seatNumber) toUpdate.AirlineTicket_seatNumber = req.body.AirlineTicket_seatNumber;
-        let result = await toUpdate.save();
-        console.log("Sucess " + result)
-        res.send(result)
+    let toUpdate = await AirlineTicket.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.passengerName)
+    toUpdate.passengerName = req.body.passengerName;
+    if(req.body.ticketNumber) toUpdate.ticketNumber = req.body.ticketNumber;
+    if(req.body.seatNumber) toUpdate.seatNumber = req.body.seatNumber;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
     } catch (err) {
-        res.status(500)
-        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
     }
-};
+   };
