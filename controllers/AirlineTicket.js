94623bc3eb643c522301dd2ce538d93cf4310d55
ShivaniPandula <143038757+ShivaniPandula@users.noreply.github.com>
2023-11-10 01:04:57 -0600

    part 2

--- controllers/AirlineTicket.js
+++ controllers/AirlineTicket.js
@@ -75,3 +75,23 @@ exports.AirlineTicket_detail = async function (req, res) {
         res.send(`{"error": document for id ${req.params.id} not found`);
     }
 };
+
+//Handle AirlineTicket update form on PUT
+exports.AirlineTicket_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await AirlineTicket.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.AirlineTicket_passengerName)
+            toUpdate.AirlineTicket_passengerName = req.body.AirlineTicket_passengerName;
+        if (req.body.AirlineTicket_ticketNumber) toUpdate.AirlineTicket_ticketNumber = req.body.AirlineTicket_ticketNumber;
+        if (req.body.AirlineTicket_seatNumber) toUpdate.AirlineTicket_seatNumber = req.body.AirlineTicket_seatNumber;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+};
