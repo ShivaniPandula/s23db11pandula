85e682f89daee37520261c062460f638b7aa58d6
ShivaniPandula <143038757+ShivaniPandula@users.noreply.github.com>
2023-11-12 13:46:12 -0600

    part5

--- controllers/AirlineTicket.js
+++ controllers/AirlineTicket.js
@@ -124,3 +124,17 @@ exports.AirlineTicket_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.AirlineTicket_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('AirlineTicketcreate', { title: 'AirlineTicket Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+};
